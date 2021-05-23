48119b186d0c77dd21483c46e7245e4721a6df1c
Abhishek Tilwar <abhishektilwar@gmail.com>
2021-05-23 21:49:55 +0530

    Create Q1.cpp

---
+++ Cook Off/Q1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int t,D,d,A,B,C;
+    cin>>t;
+    while(t--){
+        cin>>D>>d>>A>>B>>C;
+        int ds=D*d;
+        if(ds<10){
+            cout<<0<<endl;
+        }
+        else if(ds>=10 && ds<21){
+            cout<<A<<endl;
+        }
+        else if(ds>=21 && ds<42){
+            cout<<B<<endl;
+        }
+        else { cout<<C<<endl;
+        }
+
+    }
+   
+    return 0;
+}
